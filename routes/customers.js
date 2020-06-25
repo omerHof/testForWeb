023254be30e7204b174cad9448b7b49b8210caa6
ohoff <ohoffman93@gmail.com>
2020-06-25 23:46:09 +0300

    first commit

---
+++ routes/customers.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+
+router.get("/", function (req, res) {
+  res.send(req.originalUrl);
+});
+
+// /customers/login
+router.get("/login", function (req, res) {
+  res.send('ok')
+});
+
+
+
+module.exports = router;
